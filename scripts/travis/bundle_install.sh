16f85ebbc979c50ced4926889385ec4c7f6769f7
Kevin Bridges <kevin@cyberswat.com>
2014-04-04 23:19:50 -0600

    Removing packer download as a travis step.

--- scripts/travis/bundle_install.sh
+++ scripts/travis/bundle_install.sh
@@ -7,6 +7,7 @@ cd ~
 wget -O "remote_$FILE_NAME" "https://$AWS_S3_BUCKET.s3.amazonaws.com/$FILE_NAME" && tar -xf "remote_$FILE_NAME"
 wget -O "remote_$FILE_NAME.sha2" "https://$AWS_S3_BUCKET.s3.amazonaws.com/$FILE_NAME.sha2"
 ls -la
-wget --no-check-certificate https://dl.bintray.com/mitchellh/packer/0.5.1_linux_amd64.zip && unzip -d packer 0.5.1_linux_amd64.zip
+wget --no-check-certificate https://dl.bintray.com/mitchellh/packer/$PACKER_FILENAME= && unzip -d packer $PACKER_FILENAME
+mv packer .bundle
 
 exit 0

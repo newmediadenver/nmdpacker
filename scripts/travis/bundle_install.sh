357fb3b710d5e5557e2ea545e26bd50ebb6be0b9
Kevin Bridges <kevin@cyberswat.com>
2014-04-04 23:34:14 -0600

    debugging output

--- scripts/travis/bundle_install.sh
+++ scripts/travis/bundle_install.sh
@@ -7,9 +7,7 @@ cd ~
 wget -O "remote_$FILE_NAME" "https://$AWS_S3_BUCKET.s3.amazonaws.com/$FILE_NAME" && tar -xf "remote_$FILE_NAME"
 wget -O "remote_$FILE_NAME.sha2" "https://$AWS_S3_BUCKET.s3.amazonaws.com/$FILE_NAME.sha2"
 wget --no-check-certificate https://dl.bintray.com/mitchellh/packer/$PACKER_FILENAME && unzip -d packer $PACKER_FILENAME
-ls -la .bundle
-ls -la
 mv packer .bundle
-ls -la .bundle
+ls -la .bundle/packer
 
 exit 0

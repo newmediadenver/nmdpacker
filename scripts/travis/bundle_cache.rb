571cfd565956e52dbc8e7cf58ada35f5832c5b90
Kevin Bridges <kevin@cyberswat.com>
2014-04-06 12:24:56 -0600

    Getting ready to package a travis cache gem.

--- scripts/travis/bundle_cache.rb
+++ scripts/travis/bundle_cache.rb
@@ -2,79 +2,120 @@
 
 require "digest"
 require "fog"
+require "logger"
 
-bucket_name     = ENV["AWS_S3_BUCKET"]
-architecture    = `uname -m`.strip
-
-file_name       = "#{ENV['BUNDLE_ARCHIVE']}-#{architecture}.tgz"
-file_path       = File.expand_path("~/#{file_name}")
-lock_file       = File.join(File.expand_path(ENV["TRAVIS_BUILD_DIR"]), "Gemfile.lock")
-digest_filename = "#{file_name}.sha2"
-old_digest      = File.expand_path("~/remote_#{digest_filename}")
-
-puts "Checking for changes"
-file = File.open(lock_file, "rb")
-contents = file.read
-
-bundle_digest = Digest::SHA2.hexdigest("#{contents}#{ENV['PACKER_FILENAME']}")
-old_digest    = File.exists?(old_digest) ? File.read(old_digest) : ""
-
-if bundle_digest == old_digest
-  puts "=> There were no changes, doing nothing"
-
-else
-  if old_digest == ""
-    puts "=> There was no existing digest, uploading a new version of the archive"
-  else
-    puts "=> There were changes, uploading a new version of the archive"
-    puts "  => Old checksum: #{old_digest}"
-    puts "  => New checksum: #{bundle_digest}"
+class TravisS3Cache
+  def initialize(args)
+    @logger = Logger.new(STDOUT)
+    @logger.level = Logger::DEBUG
+    @storage = Fog::Storage.new({
+      :provider => "AWS",
+      :aws_access_key_id => args['aws_s3_key'],
+      :aws_secret_access_key => args['aws_s3_secret'],
+      :region => args['aws_s3_region']
+    })
+    @extras = args['aws_s3_extras']
+    @bucket = args['aws_s3_bucket']
+    @archive = args['bundle_archive']
+    @lock_file = '/home/cyberswat/nmdpacker/Gemfile.lock'
+    @previous_sha2 = '/home/cyberswat/nmdpacker/remote_nmdpacker-x86_64.tgz.sha2'
+    @filename = "#{args['bundle_archive']}-#{`uname -m`.strip}.tgz"
+    @changed = nil
   end
 
-  puts "=> Preparing bundle archive cd ~ && tar -cjf #{file_name} .bundle && split -b 5m -a 3 #{file_name} #{file_name}"
-  `cd ~ && tar -cjf #{file_name} .bundle && split -b 5m -a 3 #{file_name} #{file_name}.`
-
-  parts_pattern = File.expand_path(File.join("~", "#{file_name}.*"))
-  parts = Dir.glob(parts_pattern).sort
-
-  storage = Fog::Storage.new({
-    :provider => "AWS",
-    :aws_access_key_id => ENV["AWS_S3_KEY"],
-    :aws_secret_access_key => ENV["AWS_S3_SECRET"],
-    :region => ENV["AWS_S3_REGION"] || "us-east-1"
-  })
-
-  puts "=> Uploading the bundle"
-  puts "  => Beginning multipart upload"
-  response = storage.initiate_multipart_upload bucket_name, file_name, { "x-amz-acl" => "public-read" }
-  upload_id = response.body['UploadId']
-  puts "    => Upload ID: #{upload_id}"
-
-  part_ids = []
-
-  puts "  => Uploading #{parts.length} parts"
-  parts.each_with_index do |part, index|
-    part_number = (index + 1).to_s
-    puts "    => Uploading #{part}"
-    File.open part do |part_file|
-      response = storage.upload_part bucket_name, file_name, upload_id, part_number, part_file
-      part_ids << response.headers['ETag']
-      puts "      => Uploaded"
+  def digest
+    contents = File.open(@lock_file, "rb").read
+    @extras.each do |extra|
+      contents << extra
     end
+    Digest::SHA2.hexdigest("#{contents}")
   end
 
-  puts "  => Completing multipart upload"
-  storage.complete_multipart_upload bucket_name, file_name, upload_id, part_ids
+  def changed?
+    if @changed.nil?
+      file = @storage.directories.get(@bucket).files.get("#{@filename}.sha2")
+      previous_hash = file.nil? ? '' : file.body
+      @logger.debug("previous_hash = #{previous_hash}")
+      @logger.debug("current_hash = #{digest}")
+      @changed = previous_hash == digest ? false : true
+    end
+    @changed
+  end
 
-  puts "=> Uploading the digest file"
-  bucket = storage.directories.new(key: bucket_name)
-  bucket.files.create({
-    :body         => bundle_digest,
-    :key          => digest_filename,
-    :public       => true,
-    :content_type => "text/plain"
-  })
+  def download
+    @logger.info("Downloading the cache from s3.")
+    @logger.debug("Write /home/cyberswat/nmdpacker/s3_#{@filename} from s3.")
+    local_file = File.open("/home/cyberswat/nmdpacker/s3_#{@filename}", "w")
+    file = @storage.directories.get(@bucket).files.get(@filename)
+    local_file.write(file.body)
+    local_file.close
+  end
+
+  def upload
+    parts = Dir.glob("/home/cyberswat/nmdpacker/#{@filename}.*").sort
+    @logger.info("Performing a #{parts.length} piece multipart journey to s3.")
+
+    part_ids = []
+    response = @storage.initiate_multipart_upload @bucket, @filename, { "x-amz-acl" => "public-read" }
+    upload_id = response.body['UploadId']
+
+    parts.each_with_index do |part, index|
+      part_number = (index + 1).to_s
+      @logger.debug("#{part_number} #{part}")
+      File.open part do |part_file|
+        response = @storage.upload_part @bucket, @filename, upload_id, part_number, part_file
+        part_ids << response.headers['ETag']
+      end
+      `rm -f #{part}`
+    end
+    @logger.info("Completing the multipart journey.")
+    @storage.complete_multipart_upload @bucket, @filename, upload_id, part_ids
+
+  end
+
+  def digest_save
+    @logger.info("Store digest in #{@filename}.sha2 on s3: #{digest}")
+    bucket = @storage.directories.new(key: @bucket)
+    bucket.files.create({
+      :body         => digest,
+      :key          => "#{@filename}.sha2",
+      :public       => true,
+      :content_type => "text/plain"
+    })
+  end
+
+  def install
+    @logger.info("Installing the cache.")
+    @logger.debug("tar -xf s3_#{@filename}")
+    `tar -xf s3_#{@filename}`
+    @logger.debug("rm -f s3_#{@filename}")
+    `rm -f s3_#{@filename}`
+  end
+
+  def package
+    @logger.info("Creating the cache and preparing it for a multipart journey to s3.")
+    @logger.debug("tar -cjf #{@filename} .bundle && split -b 5m -a 3 #{@filename} #{@filename}.")
+    `tar -cjf #{@filename} .bundle && split -b 5m -a 3 #{@filename} #{@filename}.`
+  end
+
 end
 
-puts "All done now."
+state = ARGV[0].nil? ? 'before' : 'after'
+cache = TravisS3Cache.new({
+  'aws_s3_key' => 'AKIAIOUXGDMFHNZ6GHUQ',
+  'aws_s3_secret' => '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',
+  'aws_s3_bucket' => 'nmd-cache',
+  'aws_s3_region' => 'us-east-1',
+  'aws_s3_extras' => ['https://dl.bintray.com/mitchellh/packer/0.5.1_linux_amd64.zip'],
+  'bundle_archive' => 'nmdpacker'
+})
+
+if state == 'before'
+  cache.download
+  cache.install
+else
+  cache.package if cache.changed?
+  cache.upload if cache.changed?
+  cache.digest_save if cache.changed?
+end
 exit 0
--- scripts/travis/bundle_install.sh
+++ scripts/travis/bundle_install.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
-ARCHITECTURE=`uname -m`
-FILE_NAME="$BUNDLE_ARCHIVE-$ARCHITECTURE.tgz"
-cd ~
-wget -O "remote_$FILE_NAME" "https://$AWS_S3_BUCKET.s3.amazonaws.com/$FILE_NAME" && tar -xf "remote_$FILE_NAME"
-wget -O "remote_$FILE_NAME.sha2" "https://$AWS_S3_BUCKET.s3.amazonaws.com/$FILE_NAME.sha2"
 if [ ! -f ~/.bundle/packer/packer ]; then
-  wget --no-check-certificate https://dl.bintray.com/mitchellh/packer/$PACKER_FILENAME && unzip -d packer $PACKER_FILENAME
+  wget --no-check-certificate https://dl.bintray.com/mitchellh/packer/0.5.1_linux_amd64.zip && unzip -d packer 0.5.1_linux_amd64.zip
   mv packer .bundle
+  rm -f 0.5.1_linux_amd64.zip
 fi
 
 exit 0

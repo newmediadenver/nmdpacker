6df748fecd429e8af1b27145a12298ccfd690f55
Kevin Bridges <kevin@cyberswat.com>
2014-04-06 06:48:00 -0600

    Correcting bash syntax.

--- scripts/travis/bundle_install.sh
+++ scripts/travis/bundle_install.sh
@@ -7,7 +7,7 @@ cd ~
 wget -O "remote_$FILE_NAME" "https://$AWS_S3_BUCKET.s3.amazonaws.com/$FILE_NAME" && tar -xf "remote_$FILE_NAME"
 wget -O "remote_$FILE_NAME.sha2" "https://$AWS_S3_BUCKET.s3.amazonaws.com/$FILE_NAME.sha2"
 
-if [ ! -f $PWD/.bundle/packer/packer ] then
+if [ ! -f $PWD/.bundle/packer/packer ]; then
   wget --no-check-certificate https://dl.bintray.com/mitchellh/packer/$PACKER_FILENAME && unzip -d packer $PACKER_FILENAME
   mv packer .bundle
 fi

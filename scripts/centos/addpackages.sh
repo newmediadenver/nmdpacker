4284747f487a188069f85eff7d07ccb4f0822628
David Arnold <david@newmediadenver.com>
2014-04-17 11:06:09 -0600

    removed addpackages.sh packages all in update.sh

--- scripts/centos/addpackages.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-yum install -y db4-devel libXpm-devel libicu-devel openldap-devel freetds-devel unixODBC-devel postgresql-devel pspell-devel net-snmp-devel libtidy-devel httpd-devel bzip2-devel libc-client-devel freetype-devel gmp-devel libjpeg-devel krb5-devel libmcrypt-devel libpng-devel openssl-devel t1lib-devel mhash-devel libxml2-devel libxslt-devel

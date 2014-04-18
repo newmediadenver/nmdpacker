f60a86b190942420996db944cf0841896f21b351
David Arnold <david@newmediadenver.com>
2014-04-18 10:17:15 -0600

    debug bundle_install.sh continues

--- scripts/travis/bundle_install.sh
+++ scripts/travis/bundle_install.sh
@@ -3,7 +3,7 @@ if [ ! -f ~/vendor/bundle/packer/packer ]; then
   wget --no-check-certificate https://dl.bintray.com/mitchellh/packer/0.5.1_linux_amd64.zip && unzip -d packer 0.5.1_linux_amd64.zip
   pwd
   echo 'look here'
-  ls -al
+  ls -al ~/vendor/bundle
   mkdir -p ~/vendor/bundle
   mv packer ~/vendor/bundle
   rm -f 0.5.1_linux_amd64.zip

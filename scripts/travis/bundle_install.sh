d5e75f3d5c2e10e18decf65b1c3f9daef93c53eb
David Arnold <david@newmediadenver.com>
2014-04-18 10:13:29 -0600

    debug bundle_install.sh

--- scripts/travis/bundle_install.sh
+++ scripts/travis/bundle_install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 if [ ! -f ~/vendor/bundle/packer/packer ]; then
   wget --no-check-certificate https://dl.bintray.com/mitchellh/packer/0.5.1_linux_amd64.zip && unzip -d packer 0.5.1_linux_amd64.zip
   pwd

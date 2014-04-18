aa893462446856748ca979f5fb8ab1bbcbaa7325
David Arnold <david@newmediadenver.com>
2014-04-18 10:25:26 -0600

    Added global variable BUNDLE_PATH

--- scripts/travis/bundle_install.sh
+++ scripts/travis/bundle_install.sh
@@ -2,7 +2,6 @@
 if [ ! -f ~/vendor/bundle/packer/packer ]; then
   wget --no-check-certificate https://dl.bintray.com/mitchellh/packer/0.5.1_linux_amd64.zip && unzip -d packer 0.5.1_linux_amd64.zip
   pwd
-  echo 'look here'
   ls -al ~/vendor/bundle
   mkdir -p ~/vendor/bundle
   mv packer ~/vendor/bundle

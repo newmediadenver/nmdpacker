17def30740bc4cf92ff76c3f4b461a6757db06cc
Kevin Bridges <kevin@cyberswat.com>
2014-05-05 15:23:18 -0600

    Updating some paths for travis.

--- scripts/travis/bundle_install.sh
+++ scripts/travis/bundle_install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -x
-if [ ! -f ~/vendor/bundle/packer/packer ]; then
+if [ ! -f nmdpacker/vendor/bundle/packer/packer ]; then
   wget --no-check-certificate https://dl.bintray.com/mitchellh/packer/0.5.1_linux_amd64.zip && unzip -d packer 0.5.1_linux_amd64.zip
   pwd
-  mkdir -p ~/vendor/bundle
-  ls -al ~/vendor/bundle
-  mv packer ~/vendor/bundle/
+  mkdir -p nmdpacker/vendor/bundle
+  ls -al nmdpacker/vendor/bundle
+  mv packer nmdpacker/vendor/bundle/
+  ls -al nmdpacker/vendor/bundle/packer
   rm -f 0.5.1_linux_amd64.zip
 fi
 
